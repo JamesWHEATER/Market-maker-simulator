8e314a90f3e220174458c1c951809f30064cfc7f
JamesWHEATER <jameswheater2@gmail.com>
2026-08-17 13:45:38 +0800

    Delete tools directory

--- tools/parse_pdf.py
+++
@@ -1,8 +0,0 @@
-from pdfminer.high_level import extract_text
-from pathlib import Path
-import sys
-p = Path(sys.argv[1])
-out = Path(sys.argv[2])
-text = extract_text(str(p))
-out.write_text(text)
-print('wrote', out)
